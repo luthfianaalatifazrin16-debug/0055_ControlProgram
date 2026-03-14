801c8f4b63d04ea3b8aa92533b9d717ba027a838
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-14 07:57:17 +0700

    input

--- nestedif.cpp
+++ nestedif.cpp
@@ -27,4 +27,17 @@ string Status_lulus(float r)
 
 int main()
 {
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    // rerata = (nilBI + nilMT) / 2;
+    // if (rerata >= 60)
+    //    status = "Lolos";
+    // else
+    //    status = "Gagal";
 }
